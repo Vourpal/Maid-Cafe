ba4de81943cb0a7fa524fa41dbc449a8cb3b6541
Bruno Oros <hollowxdstorm@gmail.com>
2026-03-20 12:26:48 -0400

    fixed cors issue

--- server/main.py
+++ server/main.py
@@ -12,6 +12,7 @@ app = Flask(__name__)
 CORS(app, supports_credentials=True, origins=[
     "http://localhost:3000",
     "http://192.168.4.103:3000"
+    "https://maid-cafe-gxuv.vercel.app"
 ])
 
 # Register blueprints
